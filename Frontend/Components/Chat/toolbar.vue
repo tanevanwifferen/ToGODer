3cf58a5eda893283dd635c8b3efd7f4fb866ed4a
Tane van Wifferen <tanevanwifferen@gmail.com>
2024-06-15 13:29:40 +0200

    color button

--- Frontend/Components/Chat/toolbar.vue
+++ Frontend/Components/Chat/toolbar.vue
@@ -9,6 +9,13 @@ const chatStore = useChatStore();
     ></v-app-bar-nav-icon>
     <v-app-bar-title>{{ chatStore.title }}</v-app-bar-title>
     <v-spacer></v-spacer>
-    <v-btn @click="globalStore.donateViewVisible = true"> Donate </v-btn>
+    <v-btn
+      style="margin-right: 2em"
+      color="#33b249"
+      variant="flat"
+      @click="globalStore.donateViewVisible = true"
+    >
+      Donate
+    </v-btn>
   </v-toolbar>
 </template>
