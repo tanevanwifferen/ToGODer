0a46896ecb5e55611c6e359a71c69a5a39ade920
Tane van Wifferen <tanevanwifferen@gmail.com>
2024-06-15 13:32:04 +0200

    hide donate button when no options specified

--- Frontend/Components/Chat/toolbar.vue
+++ Frontend/Components/Chat/toolbar.vue
@@ -10,6 +10,7 @@ const chatStore = useChatStore();
     <v-app-bar-title>{{ chatStore.title }}</v-app-bar-title>
     <v-spacer></v-spacer>
     <v-btn
+      v-if="globalStore.donateOptions.length > 0"
       style="margin-right: 2em"
       color="#33b249"
       variant="flat"
