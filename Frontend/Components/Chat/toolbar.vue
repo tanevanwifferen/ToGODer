b19af2015b5c5a566dec8f0d6eb565a9ac4c24a6
Tane van Wifferen <tane.van.wifferen@pads4.com>
2024-07-03 12:53:20 +0200

    add some seo

--- Frontend/Components/Chat/toolbar.vue
+++ Frontend/Components/Chat/toolbar.vue
@@ -1,13 +1,9 @@
-<script setup>
-const globalStore = useGlobalStore();
-const chatStore = useChatStore();
-</script>
 <template>
   <v-toolbar style="grid-row: 1">
     <v-app-bar-nav-icon
       @click="globalStore.sidebarVisible = !globalStore.sidebarVisible"
     ></v-app-bar-nav-icon>
-    <v-app-bar-title>{{ chatStore.title }}</v-app-bar-title>
+    <v-app-bar-title>{{ title }}</v-app-bar-title>
     <v-spacer></v-spacer>
     <v-btn
       v-if="globalStore.donateOptions.length > 0"
@@ -20,3 +16,20 @@ const chatStore = useChatStore();
     </v-btn>
   </v-toolbar>
 </template>
+
+<script>
+export default {
+  setup() {
+    const globalStore = useGlobalStore();
+    const chatStore = useChatStore();
+    return { globalStore, chatStore };
+  },
+  computed: {
+    title() {
+      return this.chatStore.title != ''
+        ? this.chatStore.title
+        : 'ToGODer - Your AI God';
+    },
+  },
+};
+</script>
