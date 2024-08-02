dd1fea782f92cae9736d9a99f70dd953329caaec
Tane van Wifferen <tanevanwifferen@gmail.com>
2024-08-02 23:44:57 +0200

    also save donation when no user found for email

--- src/Web/KoFiController.ts
+++ src/Web/KoFiController.ts
@@ -8,18 +8,13 @@ export function setupKoFi(app: Express) {
   kofi(app, {
     async onDonation(donation: DonationData, req) {
       const db = getDbContext();
-      const user = await db.user.findUnique({
-        where: { email: donation.email },
+      await db.payment.create({
+        data: {
+          amount: new Decimal(donation.amount),
+          user_email: donation.email,
+          timestamp: new Date(),
+        },
       });
-      if (user) {
-        await db.payment.create({
-          data: {
-            amount: new Decimal(donation.amount),
-            user_email: user.email,
-            timestamp: new Date(),
-          },
-        });
-      }
     },
     verificationToken: process.env.KOFI_WEBHOOK_TOKEN!,
   });
