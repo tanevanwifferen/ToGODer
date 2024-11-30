e70237c1bbe12afb1ebf4820e71ae2bd9e28c0fe
tanevanwifferen <tane@hodlgroup.com>
2024-11-30 18:38:48 +0100

    remove one prompt limitation for title

--- src/Web/Validators.ts
+++ src/Web/Validators.ts
@@ -77,9 +77,6 @@ export const validateTitleMessage = (
         'Invalid request body: Expected an object with "content" as an array of ChatCompletionMessageParam objects.'
       );
   }
-  if (body.length != 1) {
-    return res.status(400).send('Only one prompt is allowed for this endpoint');
-  }
   if (!isValidChatCompletionMessageParam(body[0])) {
     return res
       .status(400)
