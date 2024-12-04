94404c0eefb94aabb50acd0f1c75f19bed0f5956
tanevanwifferen <tane@hodlgroup.com>
2024-12-04 21:03:51 +0100

    try toml

--- src/LLM/prompts/systemprompts.ts
+++ src/LLM/prompts/systemprompts.ts
@@ -5,29 +5,49 @@ export const GetTitlePrompt =
 
 export const UpdatePersonalDataPrompt =
   '\
-  I will send you a JSON object with personal data of the user. It is an object \
-  which can have fields and array properties. Think "name":"ToGODer", "age":30\
-  but also arrays like "hobbies":["programming","reading"], or containing even \
+  I will send you a input with personal data of the user. It is an object \
+  which can have fields and array properties. Think name:"name of user", age:30\
+  but also arrays like hobbies: programming,reading, or containing even \
   more advanced objects like \
-  "goals":[{id:1,goal:"understand toGODer", priority:low, state: in_progress, actions_done:[installed app]}] \
+  "goals":\
+    - id:1\
+    - goal:"understand toGODer" \
+    - priority:low, \
+    - state: in_progress, \
+    - actions_done:\
+        - installed app \
   It is accompanied with a prompt that the user has sent, and it is your job to \
-  see if this JSON object with personal data needs to be updated. If it does, \
-  return the entire JSON object with the necessary updates applied. \
+  see if this object with personal data needs to be updated. If it does, \
+  return the entire new object with the necessary updates applied. \
   \
-  Return valid JSON, ONLY send the JSON object, no markdown things, and let me parse the object on the client side.\
+  Return valid TOML, ONLY send the TOML object, no markdown things, and let me \
+  parse the object on the client side.\
   \
   Only update a property when new information is provided.\
-  Don\'t update a property when the user asks a question, return the original object then.\
-  \
-  If the input object contains anything weird, unreadable, feel free to replace it with something more readable.\
-  You are the boss here, so you should be able to parse it. If there\'s some weird arrays or items in there, \
-  feel free to delete the items. But parsing and restructuring would be much better.\
+  Don\'t update a property when the user asks a question, return the original \
+  object then.\
   \
   Also feel free to add properties if you see anything needed. For example, if you see \
-  a goal as {id, goal, priority, state, actions_done} and you think it would be better to also have \
-  a deadline, or a property "steps_to_take" or "next_action", feel free to add it.\
+  a goal as {id, goal, priority, state, actions_done} and you think it would be better \
+  to also have a deadline, or a property "steps_to_take" or "next_action", feel free to \
+  add it.\
   \
+  Also, if you notice something is goin on in the users\' life, feel free to add a date \
+  for that event. I will also send the current date as a system message, so you can use that.\
   \
-  If there are no changes, you can send "{} ". Nothing else. Don\'t send any filler text, just "{}" \
-  or the modified version of the JSON object.\
+  Important is to keep it short and to the point. Don\'t go on windy tangents, it should contain \
+  only the necessary information, in keywords, or at most a short sentence. No longer than \
+  goal:\
+    - quit_smoking\
+      - last_cigarette: 2024-01-01\
+      - next_action: throw away all cigarettes\
+      - actions\
+        - throw away all cigarettes\
+        - clean ashtrays\
+        - find coping mechanisms\
+      - actions done\
+        - read alan carr \
+  \
+  If there are no changes, you can send empty string "". Nothing else. Don\'t send any \
+  filler text, just "" or the modified version of the TOML object.\
 ';
