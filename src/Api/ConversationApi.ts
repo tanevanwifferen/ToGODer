6ab56c54fe271f1e9e30a5bb4e509f8d9d6c4e44
tanevanwifferen <tane@hodlgroup.com>
2024-12-15 16:04:01 +0100

    Revert "oops"
    This reverts commit 98b29973d98eda0fde7b9f0533e14ed25cfcb8c2.

--- src/Api/ConversationApi.ts
+++ src/Api/ConversationApi.ts
@@ -118,8 +118,11 @@ export class ConversationApi {
     }
 
     const wrapper = this.getAIWrapper(getDefaultJSONModel(), user);
-    var prompts: any = body.prompts.map((x) => ({ ...x, role: 'system' }));
-    const json_response = await wrapper.getJSONResponse(memoryPrompt, prompts);
+    var prompts = body.prompts.map((x) => ({ ...x, role: 'system' }));
+    const json_response = await wrapper.getJSONResponse(
+      memoryPrompt,
+      body.prompts
+    );
     const content = CompletionToContent(json_response);
     console.log('request memory:', json_response);
     if ((await json_response).usage?.total_tokens == 0) {
